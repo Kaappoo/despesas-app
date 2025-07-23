8b812c90d27c796426e92aa10e01b0509cd79a68
Kaappoo <kaua.ka373@gmail.com>
2025-07-22 23:25:01 -0300

    feat: create json file if not exist

--- server.js
+++ server.js
@@ -20,16 +20,31 @@ app.post('/salvar-dados', (req, res) => {
 
 app.get('/buscar-dados', (req, res) => {
   readFile('./dados.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Erro ao ler:', err);
-      return res.status(500).send('Falha ao ler os dados');
-    }
-    try {
-      const parsedData = JSON.parse(data);
-      res.json(parsedData);
-    } catch (parseError) {
-      console.error('Erro ao fazer parse:', parseError);
-      res.status(500).send('Erro ao interpretar os dados');
+    if(err){
+      if (err.code != 'ENOENT') {
+        console.error('Erro ao ler:', err);
+        return res.status(500).send('Falha ao ler os dados');
+      }
+      if (err.code == 'ENOENT') {
+        console.log('Arquivo dados.json não encontrado, criando...');
+        const initialData = {salary: 100, dispenses: []}; // Conteúdo inicial para o arquivo (array vazio)
+        writeFile('./dados.json', JSON.stringify(initialData), (err) => {
+          if(err) {
+            console.error('Erro ao criar o arquivo dados.json:', writeError);
+            return res.status(500).send('Falha ao criar o arquivo de dados');
+          }
+          console.log('Arquivo dados.json criado com sucesso.');
+          res.json(initialData); // Retorna os dados iniciais vazios
+        });
+      }
+    } else {
+      try {
+        const parsedData = JSON.parse(data);
+        res.json(parsedData);
+      } catch (parseError) {
+        console.error('Erro ao fazer parse:', parseError);
+        res.status(500).send('Erro ao interpretar os dados');
+      }
     }
   });
 });
