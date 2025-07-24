99c261f6c80f3cf5491cd6b0f27380cb0874b58a
Kaappoo <kaua.ka373@gmail.com>
2025-07-23 21:13:27 -0300

    refactor server.js

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const PORT = 3001;
 app.use(cors());
 app.use(json());
 
-app.post('/salvar-dados', (req, res) => {
+app.post('/salvar-dados', (req, res) => writeJsonData(req, res));
+
+app.get('/buscar-dados', (req, res) =>  readJsonData(req, res));
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+const writeJsonData = (req, res) => {
   const dados = req.body;
   writeFile('./dados.json', JSON.stringify(dados, null, 2), (err) => {
     if (err) {
@@ -15,40 +23,33 @@ app.post('/salvar-dados', (req, res) => {
       return res.status(500).send('Falha ao salvar os dados');
     }
     res.send('Dados salvos com sucesso!');
-  });
-});
+  })
+}
 
-app.get('/buscar-dados', (req, res) => {
+const readJsonData = (req, res) => {
   readFile('./dados.json', 'utf8', (err, data) => {
-    if(err){
-      if (err.code != 'ENOENT') {
-        console.error('Erro ao ler:', err);
-        return res.status(500).send('Falha ao ler os dados');
-      }
-      if (err.code == 'ENOENT') {
-        console.log('Arquivo dados.json não encontrado, criando...');
-        const initialData = {salary: 100, dispenses: []}; // Conteúdo inicial para o arquivo (array vazio)
-        writeFile('./dados.json', JSON.stringify(initialData), (err) => {
-          if(err) {
-            console.error('Erro ao criar o arquivo dados.json:', writeError);
-            return res.status(500).send('Falha ao criar o arquivo de dados');
-          }
-          console.log('Arquivo dados.json criado com sucesso.');
-          res.json(initialData); // Retorna os dados iniciais vazios
-        });
-      }
-    } else {
-      try {
-        const parsedData = JSON.parse(data);
-        res.json(parsedData);
-      } catch (parseError) {
-        console.error('Erro ao fazer parse:', parseError);
-        res.status(500).send('Erro ao interpretar os dados');
-      }
+    if (err && err.code != 'ENOENT') {
+      console.error('Erro ao ler:', err);
+      return res.status(500).send('Falha ao ler os dados');
+    } 
+    if (err?.code == 'ENOENT') {
+      console.log('Arquivo dados.json não encontrado, criando...');
+      const initialData = {salary: 100, dispenses: []};
+      return writeFile('./dados.json', JSON.stringify(initialData), (err) => {
+        if(err) {
+          console.error('Erro ao criar o arquivo dados.json:', writeError);
+          res.status(500).send('Falha ao criar o arquivo de dados');
+        }
+        console.log('Arquivo dados.json criado com sucesso.');
+        res.json(initialData);
+      });
+    }
+    try {
+      const parsedData = JSON.parse(data);
+      res.json(parsedData);
+    } catch (parseError) {
+      console.error('Erro ao fazer parse:', parseError);
+      res.status(500).send('Erro ao interpretar os dados');
     }
   });
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+}
